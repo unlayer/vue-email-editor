f97b1f8685edc2b9d96cc2bf844c4d25c7ed2a4a
Umair Siddique <umairsiddique@gmail.com>
2018-10-29 15:28:41 -0700

    Added initial code for email editor component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <EmailEditor :projectId="1" :templateId="59" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmailEditor from './components/EmailEditor.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    EmailEditor
   }
 }
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+#app .container {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 100%;
 }
 </style>
---
+++ src/components/EmailEditor.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <div id="editor"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmailEditor',
+  props: {
+    projectId: Number,
+    templateId: Number,
+  },
+  created() {
+    const unlayerScript = document.createElement('script');
+    unlayerScript.setAttribute('src', '//editor.unlayer.com/embed.js');
+    unlayerScript.onload = () => {
+      this.loadEditor();
+    };
+    document.head.appendChild(unlayerScript);
+  },
+  mounted() {
+  },
+  methods: {
+    loadEditor() {
+      unlayer.init({
+        id: 'editor',
+        projectId: this.projectId,
+        templateId: this.templateId,
+        displayMode: 'email',
+      });
+    },
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  flex: 1;
+  display: flex;
+}
+
+#editor {
+  flex: 1;
+  display: flex;
+
+}
+
+#editor > iframe {
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  min-height: 500px !important;
+  display: flex;
+  border: 0px;
+}
+</style>
