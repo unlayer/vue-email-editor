2126bc35266c5e5276da4a4a5532444952a7cd0b
Umair Siddique <umairsiddique@gmail.com>
2020-06-11 09:44:12 +0500

    Added vue router as a dev dependency.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.js'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: App },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App)
+  router
 }).$mount('#app')
